deca3624478f6eae89985aa5f87e5e6f44559f46
anarakinson <anarakinson@gmail.com>
2022-08-10 15:21:28 +0300

    add start.sh and gunicorn

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-run_rq() {
-  rq worker prediction_api -u 'redis://app-redis:6379' 2>&1 | tee -a &
-}
-
 run_gunicorn() {
-  gunicorn prediction_api:app --preload -b 0.0.0.0:5000 --workers=2 2>&1 | tee -a
+  gunicorn prediction_api:app --preload -b 127.0.0.1:5000 --workers=2 2>&1 | tee -a
 }
 
-run_rq
 run_gunicorn
-# python3 prediction_api.py
